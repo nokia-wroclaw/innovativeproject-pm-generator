87af1b8d12117e59523d049882a292faa267380b
Mabuza713 <uxbeix@gmail.com>
2026-04-22 20:02:30 +0000

    Set AIRFLOW_PROJ_DIR

--- tools/bin/start_airflow.sh
+++ tools/bin/start_airflow.sh
@@ -4,5 +4,6 @@
 if [[ "$(docker network ls -f -p name=shared-network 2> /dev/null)" == "" ]]; then
   docker network create shared-network
 fi
+export AIRFLOW_PROJ_DIR=../apps/airflow/
 export ENV_FILE_PATH=../.env
 docker compose --file ../../infra/airflow-docker-compose.yml up -d --build
