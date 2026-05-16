f66a10dc1b0d6e3c1b954693f711a962c66334cf
LepsyMikolaj3301 <miki2004200@wp.pl>
2026-05-16 20:29:33 +0000

    raw transform working version and logger added

--- apps/generator/preprocessing/raw_transform.py
+++ apps/generator/preprocessing/raw_transform.py
@@ -5,36 +5,18 @@ from pyspark.sql import DataFrame, Window
 from pyspark.sql import functions as f
 
 from utils.consts import SHARED_DIR_PATH, SPARK_CONFIGS
+from utils.logger import get_logger
 from utils.utils import SparkDataManager
 
-# custom config for test
-# spark_config = {
-#     "spark.log.level": "ERROR",
-#     # Driver
-#     "spark.driver.memory": "20g",
-#     "spark.driver.maxResultSize": "8g",
-#     # Executor
-#     "spark.executor.memory": "24g",
-#     "spark.executor.memoryOverhead": "6g",
-#     # Memory fractions
-#     "spark.memory.fraction": "0.8",
-#     "spark.memory.storageFraction": "0.2",
-#     # Shuffle
-#     "spark.sql.shuffle.partitions": "800",
-# }
-
+logger = get_logger()
 sdm = SparkDataManager(SPARK_CONFIGS["FULL_HEAVY"])
-
-PM_DATA_PATH = [SHARED_DIR_PATH / "raw_data" / f"pm_kpis_part{i}.parquet" for i in range(1, 6)]
-KPI_DEFINITIONS_PATH = SHARED_DIR_PATH / "raw_data" / "kpis_definitions.parquet"
-SIMPLE_REPORTS_PATH = SHARED_DIR_PATH / "raw_data" / "simple_reports.parquet"
 
 
 # PREPROCESSING_CONFIG = load_config(
 # "/home/sparkuser/app/apps/apps/generator/preprocessing/preprocessing_config.yaml")
 
 EDA_DATA_PATH = SHARED_DIR_PATH / "eda_data"
-
+PREPROCESSED_DATASET_PATH = SHARED_DIR_PATH / "preprocessed_dataset"
 
 # HELPER FUNCTIONS
 
@@ -114,7 +96,8 @@ def coalesce_kpi_version(
     kpi_definitions: DataFrame,
     chunk_size=10,
     overwrite_null_with_older_version_value=False,
-):
+    include_kpi_origin=False,
+) -> tuple[DataFrame, DataFrame]:
     def _kpi_definition_comparison(kpi_definitions: DataFrame) -> DataFrame:
         KPI_DEFINITION_VARIABLES = [
             col for col in kpi_definitions.columns if col not in ["kpi_id", "base_kpi"]
@@ -142,35 +125,13 @@ def coalesce_kpi_version(
 
         kpi_definitions_change_result = kpi_definitions_signed.groupBy(
             "base_kpi", "kpi_definition_changed"
-        ).agg(f.collect_list(f.col("kpi_id")).alias("kpi_id_list"))
+        ).agg(f.sort_array(f.collect_list(f.col("kpi_id"))).alias("kpi_id_list"))
 
         return kpi_definitions_change_result
 
     # TODO: ANALYZE HOW KPI STATISTICS CHANGE, WHEN VERSION CHANGES
 
-    # def __statistical_pm_version_comparison(pm_df_filtered: DataFrame):
-
-    #     # calculate signinficant statistics
-    #     significance_stats = {
-    #         "mean_kpi_value": f.mean,
-    #         "std_kpi_value": f.std
-    #         # Add other statistics
-    #     }
-
-    #     pm_df_stats = pm_df_filtered.groupBy("bts_id", "base_kpi", "kpi_id").agg(
-    #         *[
-    #             func(f.col("kpi_value")).alias(key)
-    #             for key, func in significance_stats.items()
-    #         ]
-    #     )
-
-    #     # TODO: Verify if there are any KPI' versions that are reverted!!!
-    #     # If there are any, orderby on kpi_id is WRONG (for some kpis)
-    #     w = Window.partitionBy("bts_id", "base_kpi").orderBy("kpi_id")
-
-    #     pm_df_stats_with_lag = pm_df_stats.withColumns(
-    #         {f"prev_{v}": f.lag(v).over(w) for v in significance_stats.keys()}
-    #     )
+    # TODO: Add empirical KPI exclusion for statistical differences
 
     print("PREPROCESSING: KPI VERSION COALESCE")
     # get only relevent kpis
@@ -188,17 +149,17 @@ def coalesce_kpi_version(
     # TODO: add other filters for kpi version mapping (maybe statistical?)
 
     # take only applicable kpis
-    mask = (
+    appliccable_kpi_mask = (
         f.col("kpi_definition_changed") == f.lit(False)
         # Add other filters here
     )
 
-    kpi_changed_definition_mapping = kpi_changed_definition_mapping.where(mask).select(
-        "base_kpi", f.posexplode(f.sort_array("kpi_id_list")).alias("version_rank", "kpi_id")
-    )
+    kpi_changed_definition_mapping_posexpl = kpi_changed_definition_mapping.where(
+        appliccable_kpi_mask
+    ).select("base_kpi", f.posexplode("kpi_id_list").alias("version_rank", "kpi_id"))
 
     pm_data_joined_mapping = pm_df_long.join(
-        kpi_changed_definition_mapping, on="kpi_id", how="left"
+        kpi_changed_definition_mapping_posexpl, on="kpi_id", how="left"
     )
 
     kpis_for_coalesce_mask = f.col("base_kpi").isNotNull()
@@ -213,7 +174,7 @@ def coalesce_kpi_version(
         .sortWithinPartitions("base_kpi")
         .persist(StorageLevel.MEMORY_AND_DISK)
     )
-
+    # Collect for persist
     pm_data_for_kpi_merge.count()
 
     base_kpis = [
@@ -245,61 +206,56 @@ def coalesce_kpi_version(
             ).alias("best")
         )
 
-        coalesced = (
-            best_rows.select("best.*")
-            .drop("version_rank", "col1")
-            .withColumnRenamed("base_kpi", "kpi_id")
-        )
+        coalesced = best_rows.select("best.*").drop("version_rank", "null_overwrite")
 
-        # coalesced = coalesced.cache()
-        # coalesced.show()
+        if include_kpi_origin:
+            coalesced = coalesced.withColumnRenamed("kpi_id", "kpi_origin")
+        else:
+            coalesced = coalesced.drop("kpi_id")
 
-        # best_rank = (
-        #     chunk_df
-        #     .filter(f.col("kpi_value").isNotNull())
-        #     .groupBy("bts_id", "distname", "start_time", "base_kpi")
-        #     .agg(f.max("version_rank").alias("best_rank"))
-        # )
+        coalesced = coalesced.withColumnRenamed("base_kpi", "kpi_id")
 
-        # # fallback: all-null groups → take newest version anyway
-        # all_ranks = (
-        #     chunk_df
-        #     .groupBy("bts_id", "distname", "start_time", "base_kpi")
-        #     .agg(f.max("version_rank").alias("max_rank"))
-        # )
-
-        # best_rank = (
-        #     all_ranks
-        #     .join(best_rank, on=["bts_id", "distname", "start_time", "base_kpi"], how="left")
-        #     .withColumn("best_rank", f.coalesce(f.col("best_rank"), f.col("max_rank")))
-        #     .drop("max_rank")
-        # )
-
-        # coalesced = (
-        #     chunk_df
-        #     .join(f.broadcast(best_rank), on=["bts_id", "distname", "start_time", "base_kpi"])
-        #     .filter(f.col("version_rank") == f.col("best_rank"))
-        #     .drop("version_rank", "best_rank", "kpi_id")
-        #     .withColumnRenamed("base_kpi", "kpi_id")
-        #     # additional checkpoint here
-        #     .checkpoint()
-        # )
-        kpi_to_find = None
-        if kpi_to_find in chunk:
-            print("MAM CIE")
-            # znajdz tu kpi ktory ma wiecej wersji + 2 wartosci sie nalozyly
         result_frames.append(coalesced)
 
-    # ── 5. Union all chunks + rest ────────────────────────────────────────────
-    df_result = reduce(lambda a, b: a.unionByName(b), result_frames + [pm_data_rest])
+    if include_kpi_origin:
+        pm_data_rest = pm_data_rest.withColumn("kpi_origin", f.col("kpi_id"))
 
-    return df_result
+    # ── 5. Union all chunks + rest
+    df_result: DataFrame = reduce(lambda a, b: a.unionByName(b), result_frames + [pm_data_rest])
+
+    # KPI definitions transform
+    kpi_changed_definition_mapping = (
+        kpi_changed_definition_mapping.withColumn(
+            "tmp_kpi_id_arr",
+            f.when(
+                appliccable_kpi_mask,
+                # Take only newest kpi definition
+                f.array(f.element_at(f.col("kpi_id_list"), -1)),
+            ).otherwise(f.col("kpi_id_list")),
+        )
+        .withColumn("kpi_id", f.explode("tmp_kpi_id_arr"))
+        .drop("base_kpi", "tmp_kpi_id_arr", "kpi_id_list")
+    )
+
+    kpi_definitions_final = kpi_definitions.join(
+        kpi_changed_definition_mapping, on="kpi_id", how="inner"
+    ).withColumn(
+        "kpi_id",
+        # overwrite kpi_id with base_kpi if appliccable
+        f.when(appliccable_kpi_mask, f.col("base_kpi")).otherwise(f.col("kpi_id")),
+    )
+
+    return df_result, kpi_definitions_final
 
 
 # Data download
 
 
 def load_data() -> tuple[DataFrame, DataFrame, DataFrame]:
+    PM_DATA_PATH = [SHARED_DIR_PATH / "raw_data" / f"pm_kpis_part{i}.parquet" for i in range(1, 6)]
+    KPI_DEFINITIONS_PATH = SHARED_DIR_PATH / "raw_data" / "kpis_definitions.parquet"
+    SIMPLE_REPORTS_PATH = SHARED_DIR_PATH / "raw_data" / "simple_reports.parquet"
+
     list_of_pm_dfs = [sdm.read_parquet(dp) for dp in PM_DATA_PATH]
 
     pm_df_long: DataFrame = reduce(lambda df1, df2: df1.unionByName(df2), list_of_pm_dfs)
@@ -310,46 +266,64 @@ def load_data() -> tuple[DataFrame, DataFrame, DataFrame]:
     return pm_df_long, kpis_definitions_df, simple_reports_df
 
 
-# PM DATA
-def raw_pm_preparing_first(pm_df_long: DataFrame) -> DataFrame:
+# pivot simple reports
+def simple_reports_pivot(simple_reports: DataFrame):
+    grouping_cols = ("datetime", "bts_id", "distname")
+    simple_reports_pivot = (
+        simple_reports.groupBy(*grouping_cols).pivot("report_name").agg(f.first("report_result"))
+    )
+
+    return simple_reports_pivot
+
+
+# raw_pm
+def raw_pm_preperation(pm_df_long: DataFrame) -> DataFrame:
     pm_df_long = pm_df_long.withColumnsRenamed({"bts_anon": "bts_id", "distname_anon": "distname"})
     pm_df_long = pm_df_long.dropDuplicates()
     pm_df_long = pm_df_long.dropna(subset=("start_time", "bts_id", "distname"))
     return pm_df_long
 
 
-def prepare_pm_data(pm_df_long: DataFrame, kpi_definitions: DataFrame):
-    print("PREPROCESSING PM DATA")
+def drop_low_coverage_kpis(pm_df_long: DataFrame, minimal_coverage: float) -> DataFrame:
+    # First drop low coverage cells
 
-    # Simple df screening
-    pm_df_long.printSchema()
-
-    pm_df_long = raw_pm_preparing_first(pm_df_long)
-
-    # KPI version flattening
-    pm_df_long = coalesce_kpi_version(pm_df_long, kpi_definitions)
-
-    # Timestamp frequency uniformoty (1 hour) and KPI-bts recording range verification
-    pm_df_long = fill_missing_timestamps(pm_df_long, "start_time", "bts_id")
-
-    sdm.write_parquet(pm_df_long, EDA_DATA_PATH / "debug_coalesced_kpi")
-    print("COALESCE SUCCESS")
-    # at last pivot
-    pm_df_wide = _pivot_pm_data(pm_df_long)
-
-    return pm_df_long, pm_df_wide
+    # TODO: Still implement this
+    pass
 
 
-def prepare_kpi_definitions(kpi_definitions_df: DataFrame) -> DataFrame:
-    # TODO: Filtering only needed kpi definitions
+def save_preprocessed_data(
+    pm_df_long: DataFrame,
+    pm_df_wide: DataFrame,
+    kpi_definitions: DataFrame,
+    simple_reports: DataFrame,
+):
+    sdm.write_parquet(
+        pm_df_long,
+        PREPROCESSED_DATASET_PATH / "pm_data_long",
+        mode="overwrite",
+        partitionBy=["kpi_id", "bts_id"],
+    )
 
-    return kpi_definitions_df
+    sdm.write_parquet(
+        pm_df_wide,
+        PREPROCESSED_DATASET_PATH / "pm_data_wide",
+        mode="overwrite",
+        partitionBy="bts_id",
+    )
 
+    sdm.write_parquet(
+        kpi_definitions,
+        PREPROCESSED_DATASET_PATH / "kpi_definitions",
+        mode="overwrite",
+        partitionBy="kpi_id",
+    )
 
-def prepare_simple_reports(simple_reports_df: DataFrame) -> DataFrame:
-    # TODO: Add simple reports filtering and report pivot?
-
-    return simple_reports_df
+    sdm.write_parquet(
+        simple_reports,
+        PREPROCESSED_DATASET_PATH / "simple_reports",
+        mode="overwrite",
+        partitionBy="bts_id",
+    )
 
 
 def main():
@@ -360,9 +334,9 @@ def main():
 
     pm_df_long_raw, kpi_definitions_df_raw, simple_reports_df_raw = load_data()
 
-    if SAVE_RAW_DATA:
-        pm_df_long_raw = raw_pm_preparing_first(pm_df_long_raw)
+    pm_df_long_raw = raw_pm_preperation(pm_df_long_raw)
 
+    if SAVE_RAW_DATA:
         sdm.write_parquet(
             pm_df_long_raw,
             EDA_DATA_PATH / "raw" / "raw_pm_data",
@@ -382,15 +356,24 @@ def main():
             pm_df_wide_raw, EDA_DATA_PATH / "raw" / "raw_pm_data_wide", mode="overwrite"
         )
 
-    pm_df_long_preprocessed, pm_df_wide_preprocessed = prepare_pm_data(
-        pm_df_long_raw, kpi_definitions_df_raw
+    # KPI version flattening
+    pm_df_long, kpi_definitions = coalesce_kpi_version(pm_df_long_raw, kpi_definitions_df_raw)
+
+    # Timestamp frequency uniformoty (1 hour) and KPI-bts recording range verification
+    pm_df_long = fill_missing_timestamps(pm_df_long, "start_time", "bts_id")
+
+    pm_df_long = pm_df_long.cache()
+    # Kpi wide format
+    pm_df_wide = _pivot_pm_data(pm_df_long)
+
+    simple_reports = simple_reports_df_raw
+
+    save_preprocessed_data(
+        pm_df_long,
+        pm_df_wide,
+        kpi_definitions,
+        simple_reports,
     )
-
-    pm_df_long_preprocessed.count()
-
-    pm_df_long_preprocessed.show()
-
-    print("DONE")
 
 
 if __name__ == "__main__":
